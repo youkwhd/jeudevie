4f248d86086df9fd9a7373bac9b5022d38253ea9
youkwhd <lolywk@tutanota.com>
2024-03-03 13:48:35 +0700

    chore: infinite loop and updates

--- lib/jeudevie.rb
+++ lib/jeudevie.rb
@@ -1,14 +1,27 @@
 require "jeudevie/cell"
 require "jeudevie/grid"
+require "jeudevie/game"
 
 class << Jeudevie
-  def simulate()
-    grid = Jeudevie::Grid::new(10, 10)
+  @grid = nil
 
-    grid.set_cell(1, 0, Jeudevie::Cell::ALIVE)
-    grid.set_cell(1, 1, Jeudevie::Cell::ALIVE)
-    grid.set_cell(1, 2, Jeudevie::Cell::ALIVE)
-    grid.alive_neighbors_count(1, 1)
-    grid.print()
+  def init()
+    @grid = Jeudevie::Grid::new(10, 10)
+    @grid.set_cell(1, 0, Jeudevie::Cell::ALIVE)
+    @grid.set_cell(1, 1, Jeudevie::Cell::ALIVE)
+    @grid.set_cell(1, 2, Jeudevie::Cell::ALIVE)
+  end
+
+  def simulate()
+    Jeudevie::init()
+
+    loop do
+      Jeudevie::update()
+      sleep 0.5
+    end
+  end
+
+  def update()
+    @grid.print()
   end
 end
